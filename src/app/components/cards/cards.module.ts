afee2cc882a1b4100e194b54075ed781074dc9e4
Pascal Iske <info@pascaliske.dev>
2022-01-11 14:27:09 +0100

    feat(time-card): add initial time card implementation

--- src/app/components/cards/cards.module.ts
+++ src/app/components/cards/cards.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { SharedModule } from 'shared/shared.module'
 import { IconModule } from '../icon/icon.module'
 import { BaseCardComponent } from './base-card/base-card.component'
+import { TimeCardComponent } from './time-card/time-card.component'
 
 @NgModule({
     imports: [SharedModule, IconModule],
-    declarations: [BaseCardComponent],
-    exports: [BaseCardComponent],
+    declarations: [BaseCardComponent, TimeCardComponent],
+    exports: [BaseCardComponent, TimeCardComponent],
 })
 export class CardsModule {}
