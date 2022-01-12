0f6915a553db3d6ba1dcd35c94e655c6804163f5
Pascal Iske <info@pascaliske.dev>
2022-01-12 13:47:40 +0100

    refactor: optimize fade in animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core'
+import { Store, select } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { AppState } from 'store'
+import { SettingsState } from 'store/settings'
+import { animations } from './home.animations'
 
 @Component({
     selector: 'cmp-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
+    animations: [animations],
 })
-export class HomeComponent {}
+export class HomeComponent {
+    public settings$: Observable<SettingsState> = this.store.pipe(select('settings'))
+
+    public constructor(private readonly store: Store<AppState>) {}
+}
