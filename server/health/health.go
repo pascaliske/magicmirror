56ed06b803328b17cee9dead33e9e598302e6b11
Pascal Iske <info@pascaliske.dev>
2022-02-09 15:37:03 +0100

    feat(server): add log line for health endpoint

--- server/health/health.go
+++ server/health/health.go
@@ -1,11 +1,15 @@
 package health
 
 import (
+	"github.com/fatih/color"
 	"github.com/hellofresh/health-go/v4"
 	"github.com/labstack/echo/v4"
+	"github.com/pascaliske/magicmirror/logger"
 )
 
 func Handler(server *echo.Echo) echo.HandlerFunc {
+	logger.Debug("Health endpoint ready at %s", color.CyanString("/health"))
+
 	h, err := health.New()
 
 	if err != nil {
