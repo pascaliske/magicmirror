e165e1d1616fd1dc4aea0b4d04905fd5b99c53f5
Pascal Iske <info@pascaliske.dev>
2022-07-05 06:43:11 +0200

    feat(news-card): implement link behind news headlines

--- src/app/components/cards/news-card/news-card.component.ts
+++ src/app/components/cards/news-card/news-card.component.ts
@@ -4,6 +4,11 @@ import { concatMap, map } from 'rxjs/operators'
 import { Cacheable } from 'ts-cacheable'
 import { BaseCardComponent, repeatAfter } from '../base-card/base-card.component'
 
+interface Headline {
+    title: string
+    link: string
+}
+
 @Component({
     selector: 'cmp-news-card',
     templateUrl: './news-card.component.html',
@@ -11,28 +16,33 @@ import { BaseCardComponent, repeatAfter } from '../base-card/base-card.component
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewsCardComponent extends BaseCardComponent {
-    public readonly data$: Observable<string[]> = this.settings$.pipe(
+    public readonly data$: Observable<Headline[]> = this.settings$.pipe(
         repeatAfter(),
         concatMap(({ data }) => this.fetchNewsData(data?.feeds ?? [])),
     )
 
     @Cacheable()
-    private fetchNewsData(urls: string[]): Observable<string[]> {
-        const feeds: Observable<string[]>[] = urls.map((url: string) => {
+    private fetchNewsData(urls: string[]): Observable<Headline[]> {
+        const feeds: Observable<Headline[]>[] = urls.map((url: string) => {
             return this.http.get(url, { responseType: 'text' }).pipe(this.extractHeadlines(4))
         })
 
         return forkJoin(feeds).pipe(map(responses => responses.flat()))
     }
 
-    private extractHeadlines(count: number): OperatorFunction<string, string[]> {
-        return map<string, string[]>((response: string) => {
+    private extractHeadlines(count: number): OperatorFunction<string, Headline[]> {
+        return map<string, Headline[]>((response: string) => {
             const parser: DOMParser = new window.DOMParser()
             const feed: Document = parser.parseFromString(response, 'text/xml')
 
-            return Array.from(feed.querySelectorAll('item > title'))
-                .map(item => item?.innerHTML ?? '')
+            return Array.from(feed.querySelectorAll('item'))
                 .slice(0, count)
+                .map(item => {
+                    const title = item?.querySelector('title')?.innerHTML ?? ''
+                    const link = item?.querySelector('link')?.innerHTML ?? ''
+
+                    return { title, link }
+                })
         })
     }
 }
